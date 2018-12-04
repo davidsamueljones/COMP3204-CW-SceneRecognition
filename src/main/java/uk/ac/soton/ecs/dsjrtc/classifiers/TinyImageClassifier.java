0201837ef2a082d1904dd5c2af09c02f2586d12d
David Jones <davidsamueljones@icloud.com>
2018-12-04 17:35:17 +0000

    Commenting and tidyup for tiny image k-means
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/uk/ac/soton/ecs/dsjrtc/classifiers/TinyImageClassifier.java
+++ src/main/java/uk/ac/soton/ecs/dsjrtc/classifiers/TinyImageClassifier.java
@@ -16,38 +16,72 @@ import org.openimaj.knn.FloatNearestNeighboursExact;
 import org.openimaj.util.pair.IntFloatPair;
 import uk.ac.soton.ecs.dsjrtc.features.TinyImageFeatureExtractor;
 
+/**
+ * Classifier that uses the tiny image feature vector with k-means classification.<br>
+ * Must be trained before classification.
+ * 
+ * @author David Jones (dsj1n15@soton.ac.uk)
+ * @author Richard Crosland (rtc1g16@soton.ac.uk)
+ */
 public class TinyImageClassifier implements TrainableClassifier<String, FImage> {
   public static final int DEFAULT_NN_K = 20;
-  public static final boolean DEFAULT_NORMALISE = true;
+  public static final boolean DEFAULT_NORMALISE = TinyImageFeatureExtractor.DEFAULT_NORMALISE;
   public static final Dimension DEFAULT_SCALE = TinyImageFeatureExtractor.DEFAULT_SCALE;
 
-  private FloatNearestNeighboursExact knn;
-  private int[] assignments;
-  private String[] groups;
-
-  /** Feature extractor */
-  private final Dimension scale;
+  /** Feature extractor used by classifier */
   private final FeatureExtractor<FloatFV, FImage> fe;
+  /** Number of neighbours considered during classification */
   private final int nNeighbours;
 
+  /** Class nearest neighbour classifier for current training set */
+  private FloatNearestNeighboursExact knn;
+  /** Group for each training point */
+  private String[] assignments;
+
+  /**
+   * Instantiate a tiny image classifier using all class defaults.
+   */
   public TinyImageClassifier() {
-    this(DEFAULT_SCALE, DEFAULT_NORMALISE, DEFAULT_NN_K);
+    this(DEFAULT_NN_K);
   }
 
-  public TinyImageClassifier(Dimension scale, boolean normalise, int nNeighbours) {
-    this.scale = scale;
+  /**
+   * Instantiate a tiny image classifier that uses with the number of considered neighbours set, and
+   * others as class defaults.
+   * 
+   * @param nNeighbours Number of neighbours to consider for classification
+   */
+  public TinyImageClassifier(int nNeighbours) {
+    this(nNeighbours, DEFAULT_SCALE, DEFAULT_NORMALISE);
+  }
+
+  /**
+   * Instantiate a tiny image classifier with all settings user set.
+   * 
+   * @param nNeighbours Number of neighbours to consider for classification
+   * @param scale Scaling to use in tiny image feature extraction
+   * @param normalise Whether normalisation should be used in tiny image feature extraction
+   */
+  public TinyImageClassifier(int nNeighbours, Dimension scale, boolean normalise) {
     this.fe = new TinyImageFeatureExtractor(scale, normalise);
     this.nNeighbours = nNeighbours;
+    this.knn = null;
   }
 
   @Override
   public ClassificationResult<String> classify(FImage img) {
+    if (knn == null) {
+      throw new IllegalStateException("Classifier has not yet been trained");
+    }
+
+    // Find the nearest neighbours of the feature vector
     float[] feature = fe.extractFeature(img).values;
     List<IntFloatPair> neighbours = knn.searchKNN(feature, nNeighbours);
-    Map<Integer, Integer> results = new HashMap<>();
+    // Collate the nearest neighbours into their groups
+    Map<String, Integer> results = new HashMap<>();
     int foundNeighbours = 0;
     for (IntFloatPair neighbour : neighbours) {
-      int group = assignments[neighbour.first];
+      String group = assignments[neighbour.first];
       Integer groupCount = results.get(group);
       if (groupCount == null) {
         groupCount = 0;
@@ -57,10 +91,11 @@ public class TinyImageClassifier implements TrainableClassifier<String, FImage> 
       // !!! TODO: Should we have a distance threshold that ignores far away neighbours?
     }
 
-    // Check results
+    // Collate results as classifications with certainty equivalent to the percentage of points that
+    // were of the found group out of all found neighbours
     BasicClassificationResult<String> classification = new BasicClassificationResult<>();
-    for (Entry<Integer, Integer> entry : results.entrySet()) {
-      classification.put(groups[entry.getKey()], entry.getValue() / (double) foundNeighbours);
+    for (Entry<String, Integer> entry : results.entrySet()) {
+      classification.put(entry.getKey(), entry.getValue() / (double) foundNeighbours);
     }
 
     return classification;
@@ -68,23 +103,20 @@ public class TinyImageClassifier implements TrainableClassifier<String, FImage> 
 
   @Override
   public void train(GroupedDataset<String, ListDataset<FImage>, FImage> trainingSet) {
-    // Instantiate assignments structures
-    groups = new String[trainingSet.size()];
-    assignments = new int[trainingSet.numInstances()];
+    // Instantiate new assignment structure for this new training set
+    assignments = new String[trainingSet.numInstances()];
 
     // Get features from each image in training set, recording its known assignment
     final float[][] data = new float[trainingSet.numInstances()][];
-    int gi = 0, ii = 0;
+    int i = 0;
     for (Entry<String, ListDataset<FImage>> group : trainingSet.entrySet()) {
-      groups[gi] = group.getKey();
       for (final FImage img : group.getValue()) {
-        data[ii] = fe.extractFeature(img).values;
-        assignments[ii] = gi;
-        ii++;
+        data[i] = fe.extractFeature(img).values;
+        assignments[i] = group.getKey();
+        i++;
       }
-      gi++;
     }
-    // Train the model
+    // Train the model with all features
     knn = new FloatNearestNeighboursExact(data);
   }
 
--- src/main/java/uk/ac/soton/ecs/dsjrtc/features/TinyImageFeatureExtractor.java
+++ src/main/java/uk/ac/soton/ecs/dsjrtc/features/TinyImageFeatureExtractor.java
@@ -6,15 +6,36 @@ import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.feature.FloatFV;
 
+/**
+ * Tiny image generator class that directly implements the feature extractor class for generic
+ * classifiers. <br>
+ * 
+ * Static image generation can be achieved using the
+ * {@link #makeTinyImage(FImage img, Dimension scale, boolean normalise)} method.
+ * 
+ * @author David Jones (dsj1n15@soton.ac.uk)
+ * @author Richard Crosland (rtc1g16@soton.ac.uk)
+ */
 public class TinyImageFeatureExtractor implements FeatureExtractor<FloatFV, FImage> {
   public static final Dimension DEFAULT_SCALE = new Dimension(4, 4);
+  public static final boolean DEFAULT_NORMALISE = true;
+
   private final Dimension scale;
   private final boolean normalise;
-  
+
+  /**
+   * Instantiate a tiny image feature extractor using all class defaults.
+   */
   public TinyImageFeatureExtractor() {
-    this(DEFAULT_SCALE, true);
+    this(DEFAULT_SCALE, DEFAULT_NORMALISE);
   }
 
+  /**
+   * Instantiate a tiny image feature extractor.
+   * 
+   * @param scale Scale for feature extractor to use for tiny image generation
+   * @param normalise Whether feature extractor should use normalisation in tiny image generation
+   */
   public TinyImageFeatureExtractor(Dimension scale, boolean normalise) {
     this.scale = scale;
     this.normalise = normalise;
@@ -26,34 +47,25 @@ public class TinyImageFeatureExtractor implements FeatureExtractor<FloatFV, FIma
     return new FloatFV(feature);
   }
 
+  /**
+   * Create a tiny image vector from an input images to the provided configuration.
+   * 
+   * @param img Image to create the tiny image vector from
+   * @param scale Size to resize the image to before packing
+   * @param normalise Whether the image should be normalised
+   * @return The packed tiny image vector, length will be scale.width * scale.height
+   */
   public static float[] makeTinyImage(FImage img, Dimension scale, boolean normalise) {
     // Crop to square around the centre
     final int dim = Math.min(img.width, img.height);
-    final int fullPixels = dim * dim;
     img = img.extractCenter(dim, dim);
 
+    // Apply normalisation to image
+    // TODO: Should this be done before crop OR before scale OR right at the end
     if (normalise) {
-      // Calculate zero-mean and unit length the image
-      float sum = 0;
-      float sumsq = 0;
-      for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-          final float val = img.pixels[y][x];
-          sum += val;
-          sumsq += val * val;
-        }
-      }
-      final float mean = sum / fullPixels;
-      final float var = sumsq / fullPixels - mean * mean;
-      
-      // Apply normalisation
-      for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-          img.pixels[y][x] = (img.pixels[y][x] - mean) / var;
-        }
-      }
+      inplaceNormalise(img);
     }
-    
+
     // Resize to tiny image scale
     img.processInplace(new ResizeProcessor(scale.width, scale.height));
 
@@ -61,7 +73,37 @@ public class TinyImageFeatureExtractor implements FeatureExtractor<FloatFV, FIma
     final float[] packed = img.getFloatPixelVector();
     return packed;
   }
-  
 
+  /**
+   * Inplace normalisation of the input image to account for illumination differences. Achieved by
+   * centring the mean around zero and dividing by the variance.
+   * 
+   * @param img Image to normalise in place
+   * @return Reference to the input image
+   */
+  private static FImage inplaceNormalise(FImage img) {
+    final int pixelCount = img.height * img.width;
+
+    // Calculate mean and variance of the image
+    float sum = 0;
+    float sumsq = 0;
+    for (int y = 0; y < img.height; y++) {
+      for (int x = 0; x < img.width; x++) {
+        final float val = img.pixels[y][x];
+        sum += val;
+        sumsq += val * val;
+      }
+    }
+    final float mean = sum / pixelCount;
+    final float var = sumsq / pixelCount - mean * mean;
+
+    // Remove the mean intensity and scale it by its intensity variance
+    for (int y = 0; y < img.height; y++) {
+      for (int x = 0; x < img.width; x++) {
+        img.pixels[y][x] = (img.pixels[y][x] - mean) / var;
+      }
+    }
+    return img;
+  }
 
 }
