311375034c4ca0d03a8cc3cb21e847fabceaaec4
David Jones <davidsamueljones@icloud.com>
2018-12-12 18:55:50 +0000

    Corrected normalisation

--- src/main/java/uk/ac/soton/ecs/dsjrtc/lib/FeatureUtilities.java
+++ src/main/java/uk/ac/soton/ecs/dsjrtc/lib/FeatureUtilities.java
@@ -1,6 +1,7 @@
 package uk.ac.soton.ecs.dsjrtc.lib;
 
 import org.openimaj.image.FImage;
+import org.openimaj.image.processing.algorithm.MeanCenter;
 
 /**
  * Utilities directly applicable to feature generation.
@@ -10,58 +11,40 @@ import org.openimaj.image.FImage;
  */
 public class FeatureUtilities {
 
-  /**
-   * Normalise a 1D array of floats. See {@link #normalise(float[][])}.
-   * @param img Image to normalise in place
-   * @return Reference to the input pixels
-   */
-  public static float[] inplaceNormalise(float[] pixels) {
-    float[][] pixels2D = {pixels};
-    inplaceNormalise(pixels2D);
-    return pixels;
-  }
-  
-  /**
-   * Normalise an image's pixels. See {@link #normalise(float[][])}.
-   * @param img Image to normalise in place
-   * @return Reference to the input pixels
-   */
-  public static FImage inplaceNormalise(FImage img) {
-    inplaceNormalise(img.pixels);
-    return img;
-  }
-  
-  /**
-   * Normalise a 2D array of floats by dividing by their variance and subtracting their mean.
-   * 
-   * @param pixels Pixels to normalise in place
-   * @return Reference to the input array
-   */
-  public static float[][] inplaceNormalise(float[][] pixels) {
-    final int height = pixels.length;
-    final int width = pixels[0].length;
+	/**
+	 * Normalise a 1D array of floats. See {@link #normalise(float[][])}.
+	 * 
+	 * @param img Image to normalise in place
+	 * @return Reference to the input pixels
+	 */
+	public static float[] inplaceNormalise(float[] pixels) {
+		float[][] pixels2D = { pixels };
+		inplaceNormalise(new FImage(pixels2D));
+		return pixels;
+	}
 
-    // Calculate mean and variance of the image
-    float sum = 0;
-    float sumsq = 0;
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        final float val = pixels[y][x];
-        sum += val;
-        sumsq += val * val;
-      }
-    }
-    final int pixelCount = height * width;
-    final float mean = sum / pixelCount;
-    final float var = sumsq / pixelCount - mean * mean;
-
-    // Remove the mean intensity and scale it by its intensity variance
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        pixels[y][x] = (pixels[y][x] - mean) / var;
-      }
-    }
-    return pixels;
-  }
+	/**
+	 * Normalise a 2D array of floats by dividing by their sum and then mean
+	 * centring.
+	 * 
+	 * @param pixels Pixels to normalise in place
+	 * @return Reference to the input array
+	 */
+	public static float[][] inplaceNormalise(float[][] pixels) {
+		inplaceNormalise(new FImage(pixels));
+		return pixels;
+	}
+	
+	/**
+	 * Normalise an image's pixels. See {@link #normalise(float[][])}.
+	 * 
+	 * @param img Image to normalise in place
+	 * @return Reference to the input pixels
+	 */
+	public static FImage inplaceNormalise(FImage img) {
+		img.normalise();
+		img.processInplace(new MeanCenter());
+		return img;
+	}
 
 }
