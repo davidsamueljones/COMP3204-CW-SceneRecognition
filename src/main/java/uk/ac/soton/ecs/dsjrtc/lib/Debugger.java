c8544176d26b4398299d0a30f3bceefda915c1a5
David Jones <davidsamueljones@icloud.com>
2018-12-13 12:15:44 +0000

    Added time output to debug

--- src/main/java/uk/ac/soton/ecs/dsjrtc/lib/Debugger.java
+++ src/main/java/uk/ac/soton/ecs/dsjrtc/lib/Debugger.java
@@ -1,5 +1,7 @@
 package uk.ac.soton.ecs.dsjrtc.lib;
 
+import org.joda.time.DateTime;
+
 /**
  * Very simple static wrapper for sending print statements that can be toggled off globally.
  * 
@@ -9,6 +11,8 @@ package uk.ac.soton.ecs.dsjrtc.lib;
 public class Debugger {
   /** Whether output is enabled for any debug reports */
   private static boolean outputEnabled = true;
+  /** Whether time should be prefixed to outputs */
+  private static boolean timeOutput = true;
 
   /**
    * This class should not be instantiated.
@@ -30,27 +34,54 @@ public class Debugger {
   }
 
   /**
-   * Print with no linefeed if output is enabled.<br>
+   * @param timeOutput Whether time should be prefixed to output
+   */
+  public static void setTimeOutput(boolean timeOutput) {
+    Debugger.timeOutput = timeOutput;
+  }
+
+  /**
+   * @return Whether time will be prefixed to output
+   */
+  public static boolean isTimeOutput() {
+    return Debugger.timeOutput;
+  }
+
+
+  /**
+   * Print with no linefeed if output is enabled. The current time will be prefixed if it is enabled
+   * globally.<br>
    * See {@link System.out#print()}.
    * 
    * @param obj Object to print
    */
   public static void print(Object obj) {
     if (Debugger.isOutputEnabled()) {
+      if (Debugger.isTimeOutput()) {
+        System.out.print(String.format("[%s]", Debugger.getTime()));
+      }
       System.out.print(obj.toString());
     }
   }
 
   /**
-   * Print with linefeed if output is enabled.<br>
-   * See {@link System.out#println()}.
+   * Debug print with linefeed if output is enabled.<br>
+   * See {@link Debugger#print(Object obj)}.
    * 
    * @param obj Object to print
    */
   public static void println(Object obj) {
     if (Debugger.isOutputEnabled()) {
-      System.out.println(obj.toString());
+      Debugger.print(obj);
+      System.out.println();
     }
+  }
+
+  /**
+   * @return The current time as a string
+   */
+  public static String getTime() {
+    return DateTime.now().toString("HH:mm:ss");
   }
 
 }
